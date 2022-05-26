6e530b2a271a5ebe7b58c6e338acc152345693b7
ebereanuebunwa <70179219+ebereanuebunwa@users.noreply.github.com>
2022-05-26 23:06:34 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import re  # Import the regex module 
+
+
+def read_file_content(filename):
+    with open(filename, 'r') as fhand:
+        content = fhand.read()
+    
+    return content 
+
+
+def count_words():
+    text = read_file_content("./story.txt")
+    # make all letters lowercase
+    txt = text.lower()  
+    
+    # Eliminate punctuations using a regex.
+    # Anything that isnt a word(alphanumeric) or a whitespace will be replaced with an empty string.
+    new_txt = re.sub(r'[^\w\s]', '', txt)
+
+    counter = {}  # a dictionary with key: word in textfile and value: value count of word.
+    for i in new_txt.split():
+        counter[i] = counter.get(i, 0) + 1
+ 
+    return counter
+
+
